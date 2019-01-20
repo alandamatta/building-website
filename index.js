950473da9b7330af5cf86f5bb6fdf3ee22c4e8ef
Alan da Matta <alanmatta@vitai.onmicrosoft.com>
2019-01-20 03:06:47 -0200

    done

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) =>{
+    res.render('home');
+});
+
+app.get('*', (req, res) =>{
+    res.send('any page');
+});
+
+app.listen(8080);
